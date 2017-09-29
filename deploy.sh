35a382b170db848ba00d43ba7c0916135bcdf39f
freddypBbta <30932728+freddypBbta@users.noreply.github.com>
2017-09-29 10:20:06 -0500

    Ajustes en archivos para despliegue

--- deploy.sh
+++ deploy.sh
@@ -6,48 +6,60 @@ JQ="jq --raw-output --exit-status"
 APP_NAME=$1
 CLUSTER_NAME=$2
 ENVIRONMENT=$3
-SECRETS_FILES_COUNT=$4
-SECRET_FILE_ID=$5 
-VERSION=$6
+VERSION=$4
 
 
-SERVICE_NAME="$ENVIRONMENT-accountopen-$APP_NAME-service"
-CLUSTER_NAME="bdb-accountopen-$ENVIRONMENT-$CLUSTER_NAME-cluster"
+SERVICE_NAME="$ENVIRONMENT-incredibles-$APP_NAME-service"
+CLUSTER_NAME="bdb-$ENVIRONMENT-incredibles-cluster"
 BUILD_NUMBER=${CIRCLE_BUILD_NUM}
 IMAGE_TAG=${CIRCLE_SHA1}
-TASK_FAMILY="$ENVIRONMENT-accountopen-$APP_NAME-task-family"
-SECRETS_FILE="$BDB_CHANNEL-$SECRET_FILE_ID-keys"
+TASK_FAMILY="$ENVIRONMENT-incredibles-$APP_NAME-task-family"
+AWS_ACCOUNT_ID=070073855891
 
 configure_aws_cli(){
-	aws --version   
-	aws configure set default.region us-east-1
-	aws configure set default.output json
-    echo "Versión ----- : $VERSION"
+    aws --version   
+    aws configure set default.region us-east-1
+    aws configure set default.output json
 }
 
 push_ecr_image(){
+    eval $(aws ecr get-login --region us-east-1 --registry-ids 628007708529)
+    #--registry-ids aws_account_id
+    
+    docker build -t $ENVIRONMENT-incredibles-$APP_NAME .
+    
+    
+    echo " build image $ENVIRONMENT-incredibles-$APP_NAME"
+    
+    docker tag $ENVIRONMENT-incredibles-$APP_NAME $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ENVIRONMENT-incredibles-$APP_NAME:$VERSION
+    
+    echo " build tag $ENVIRONMENT-incredibles-$APP_NAME $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ENVIRONMENT-incredibles-$APP_NAME:$VERSION"
+    
     eval $(aws ecr get-login --region us-east-1)
     
-    docker build -t $ENVIRONMENT-$APP_NAME --build-arg BDB_ENV=$ENVIRONMENT --build-arg BDB_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg BDB_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --build-arg BDB_AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID --build-arg BDB_SECRETS_FILE_ENV=$SECRETS_FILE --build-arg BDB_SECRETS_FILES_COUNT_ENV=$SECRETS_FILES_COUNT .
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ENVIRONMENT-incredibles-$APP_NAME:$VERSION
     
-    docker tag $ENVIRONMENT-$APP_NAME $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ENVIRONMENT-$APP_NAME:$VERSION
-    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ENVIRONMENT-$APP_NAME:$VERSION
+    
 }
+
 
 deploy_cluster(){
 
-	make_task_def
-	register_task_definition
+    make_task_def
+    register_task_definition
     if  [ ${ENVIRONMENT} != "prod" ]; then
-        echo ${CLUSTER_NAME}
-    	echo ${SERVICE_NAME}
+        
 
-    	DESIRED_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | head -1 | tr "/" " " | awk '{print $2}' | sed 's/,$//')
-    	if [ ${DESIRED_COUNT} = "0" ]; then
-    	    DESIRED_COUNT="1"
-    	fi
+        DESIRED_COUNT=$(aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | head -1 | tr "/" " " | awk '{print $2}' | sed 's/,$//')
+        if [ ${DESIRED_COUNT} = "0" ]; then
+            DESIRED_COUNT="1"
+        fi
+        #DESIRED_COUNT="1"
 
 
+        echo " enviroment: $ENVIRONMENT"
+        
+        echo " services name: $SERVICE_NAME"
         tasks=$(aws --region us-east-1 ecs list-tasks --cluster $CLUSTER_NAME --family ${TASK_FAMILY} | jq -r '.taskArns | map(.[40:]) | reduce .[] as $item (""; . + $item + " ")')
         for task in $tasks; do
             aws --region us-east-1 ecs stop-task --task $task --cluster $CLUSTER_NAME
@@ -55,7 +67,7 @@ deploy_cluster(){
         done
         #aws --region us-west-2 ecs deregister-container-instance --cluster $cluster --container-instance $container_instance
 
-    	if [[ $(aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $revision --desired-count ${DESIRED_COUNT} | \
+        if [[ $(aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $revision --desired-count ${DESIRED_COUNT} | \
                        $JQ '.service.taskDefinition') != $revision ]]; then 
             echo "Error updating service."
             return 1
@@ -67,25 +79,24 @@ deploy_cluster(){
 }
 
 make_task_def(){
+    
     TASK_ALL=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY})
-    echo "Task All: $TASK_ALL"
+    
+    echo "Task All: $TASK_ALL  - TaskFamily ${TASK_FAMILY}  "
 
 
-	TASK_TEMPLATE=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | jq -r .taskDefinition.containerDefinitions[0])
+    TASK_TEMPLATE=$(aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | jq -r .taskDefinition.containerDefinitions[0])
     echo "Template anterior: $TASK_TEMPLATE"
  
-    NEW_CONTAINER_DEFINITIONS=$(echo "$TASK_TEMPLATE" | jq ".image = \"${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${ENVIRONMENT}-${APP_NAME}:${VERSION}\""  )
+    NEW_CONTAINER_DEFINITIONS=$(echo "$TASK_TEMPLATE" | jq ".image = \"%s.dkr.ecr.us-east-1.amazonaws.com/${ENVIRONMENT}-incredibles-orchestrator:%s\""  )
+  
+    echo "Template nuevo: $NEW_CONTAINER_DEFINITIONS"
 
-	# task_def=$(printf "$NEW_CONTAINER_DEFINITIONS" $AWS_ACCOUNT_ID "latest")
-    
-    #task_def=$TASK_TEMPLATE
-    task_def=$NEW_CONTAINER_DEFINITIONS
-
-    echo "Template nuevo: $task_def"    
+    task_def=$(printf "$NEW_CONTAINER_DEFINITIONS" $AWS_ACCOUNT_ID $VERSION)
 }
 
 register_task_definition() {
-	#--container-definitions "$task_def"
+    #--container-definitions "$task_def"
     if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $TASK_FAMILY | $JQ '.taskDefinition.taskDefinitionArn'); then
         echo "Revision: $revision"
     else
@@ -97,12 +108,21 @@ register_task_definition() {
 
 setup_env_amazon()
 {
+if  [ ${ENVIRONMENT} = "qa-incredibles" ]; then
+        export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID_MVP3
+        export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_MVP3
+        export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_MVP3
+    fi
+    
+    
     if  [ ${ENVIRONMENT} = "prod" ]; then
         export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID_PROD
         export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PROD
         export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PROD
     fi
 }
+
+
 
 setup_env_amazon
 configure_aws_cli
