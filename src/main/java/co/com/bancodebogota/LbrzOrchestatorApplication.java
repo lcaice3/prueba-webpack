e94e0ce9dc3429eddd15db0e3dd8b1283b092160
freddypBbta <30932728+freddypBbta@users.noreply.github.com>
2017-10-05 09:45:49 -0500

    ajueste de autowire

--- src/main/java/co/com/bancodebogota/LbrzOrchestatorApplication.java
+++ src/main/java/co/com/bancodebogota/LbrzOrchestatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
-@ComponentScan({"co.com.bancodebogota.blacklist","co.com.bancodebogota.customers","co.com.bancodebogota.simulator"})
+
 @SpringBootApplication
 public class LbrzOrchestatorApplication {
 	
